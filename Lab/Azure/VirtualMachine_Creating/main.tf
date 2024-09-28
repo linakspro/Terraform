f1b778aa63f22b8f60426e91125c9ffa8124cefd
linakspro <53853455+linakspro@users.noreply.github.com>
2024-09-28 17:14:17 +0530

    VM Creation

---
+++ Lab/Azure/VirtualMachine_Creating/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.64.0"
+    }
+  }
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+#  subscription_id = "e23bd767-459d-44e8-a3c5-5c8721a87b6c"
+}
+
+data "azurerm_client_config" "current" {}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-vm-rg"
+  location = "South India"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "terraform-public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "terraform-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "terraform-ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Create a virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "terraform-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # VM size
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_username = "adminuser"
+  admin_password = "P@ssw0rd1234!" # Change this to a secure password
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name  = "terraform-vm"
+  disable_password_authentication = false
+}
+
+# Output the public IP of the VM
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
